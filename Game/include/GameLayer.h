b8f80dbc6a68b871f477cf5069683c2685680345
Erik Holmes <erikholmesdev@gmail.com>
2026-04-29 23:49:38 -0400

    Added header comments for the GameLayer class and it's methods

--- Game/include/GameLayer.h
+++ Game/include/GameLayer.h
@@ -1,3 +1,7 @@
+//File		 : 
+//Programmer : 
+//Date		 :
+//Description: 
 #pragma once
 
 #include "Layer.h"
@@ -7,6 +11,8 @@
 
 #include <SFML/Graphics.hpp>
 
+//Class		 : GameLayer
+//Description: This class is derived from our engines layer class found in the Layer.h 
 class GameLayer : public Thrive::Layer
 {
 public: 
@@ -16,17 +22,29 @@ public:
 		
 	}
 
+	//Method		: OnUpdate (float dt)
+	//Parameters	: float dt
+	//Returns		: void
+	//Description   : This method will be used when the game layer updates
 	void OnUpdate(float dt) override
 	{
 		
 	}
 
+	//Method		: OnRender ()
+	//Parameters	: ()
+	//Returns		: void
+	//Description   : 
 	void OnRender() override
 	{
 
 	}
 
-	void OnEvent(Thrive::Event& e)override
+	//Method		: OnEvent (Thrive::Event& e)
+	//Parameters	: Thrive::Event& e 
+	//Returns		: void
+	//Description   : This method will be our OnEvent when event happenes with in the game 
+	void OnEvent(Thrive::Event& e) override
 	{
 
 	}
